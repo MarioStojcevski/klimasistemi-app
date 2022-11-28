2e141514e5e17c2f1908cf73070b032821a1ec72
Mario Stojchevski <mario@halicea.com>
2022-11-28 17:25:39 +0100

    Removed console.log

--- src/app/air-conditioner/filter-and-sort/filter-and-sort.component.ts
+++ src/app/air-conditioner/filter-and-sort/filter-and-sort.component.ts
@@ -13,10 +13,9 @@ import { IAirConditionerBrand } from 'src/app/model/air-conditioner-brand';
 @Component({
   selector: 'app-filter-and-sort',
   templateUrl: './filter-and-sort.component.html',
-  styleUrls: ['./filter-and-sort.component.scss']
+  styleUrls: ['./filter-and-sort.component.scss'],
 })
 export class FilterAndSortComponent implements OnInit {
-
   @Output() airConditionersFiltered = new EventEmitter<IAirConditioner[]>();
   @Output() size = new EventEmitter<number>();
 
@@ -31,24 +30,25 @@ export class FilterAndSortComponent implements OnInit {
     { name: '2.5 kW', value: '2.5' },
     { name: '3.5 kW', value: '3.5' },
     { name: '5.5 kW', value: '5.5' },
-    { name: '7.0 kW', value: '7.0'},
+    { name: '7.0 kW', value: '7.0' },
     { name: '8.0 kW', value: '8.0' },
     { name: '10.0 kW', value: '10.0' },
     { name: '12.0 kW', value: '12.0' },
-    { name: '15.0 kW', value: '15.0' }
+    { name: '15.0 kW', value: '15.0' },
   ];
 
   constructor(
     public fb: FormBuilder,
     private airConditionerService: AirConditionerService,
     private translate: TranslateService,
-    public mediaObserver: MediaObserver) {
+    public mediaObserver: MediaObserver
+  ) {
     this.filterDto = {
       sortBy: '',
       minPrice: 15000,
       maxPrice: 50000,
       powerArray: [],
-      filterByBrand: ""
+      filterByBrand: '',
     };
   }
 
@@ -59,10 +59,9 @@ export class FilterAndSortComponent implements OnInit {
   }
 
   private getAllBrands(): void {
-    this.airConditionerService.getAllBrands()
-      .subscribe((brands) => {
-        this.airConditionerBrands = brands.data.brands;
-      });
+    this.airConditionerService.getAllBrands().subscribe((brands) => {
+      this.airConditionerBrands = brands.data.brands;
+    });
   }
 
   private initFilterForm(): void {
@@ -71,25 +70,26 @@ export class FilterAndSortComponent implements OnInit {
       minPriceControl: new FormControl(this.minValue),
       maxPriceControl: new FormControl(this.maxValue),
       powerArrayControl: this.fb.array([]),
-      filterByBrandControl: [null]
+      filterByBrandControl: [null],
     });
   }
 
   private setDeviceSize(): void {
-    this.mediaSub = this.mediaObserver.media$.subscribe(
-      ((result) => {
-        this.deviceSmXs = result.mqAlias === 'sm' || result.mqAlias === 'xs' ? true : false;
-    }));
+    this.mediaSub = this.mediaObserver.media$.subscribe((result) => {
+      this.deviceSmXs =
+        result.mqAlias === 'sm' || result.mqAlias === 'xs' ? true : false;
+    });
   }
 
   public onCheckboxChange(e: any): void {
-    const powerArray: FormArray = this.filterForm.get('powerArrayControl') as FormArray;
+    const powerArray: FormArray = this.filterForm.get(
+      'powerArrayControl'
+    ) as FormArray;
     if (e.checked) {
       powerArray.push(new FormControl(e.source.value));
-    } 
-    else {
-      for(let i=0; i<powerArray.controls.length; i++) {
-        if(powerArray.controls[i].value == e.source.value) {
+    } else {
+      for (let i = 0; i < powerArray.controls.length; i++) {
+        if (powerArray.controls[i].value == e.source.value) {
           powerArray.removeAt(i);
         }
       }
@@ -97,39 +97,43 @@ export class FilterAndSortComponent implements OnInit {
   }
 
   public filter(): void {
-    let unFormattedPowerArray: string[] = this.filterForm.get('powerArrayControl')?.value;
-    let formattedPowerArray: number[] = unFormattedPowerArray.map(x => +x*1000);
+    let unFormattedPowerArray: string[] =
+      this.filterForm.get('powerArrayControl')?.value;
+    let formattedPowerArray: number[] = unFormattedPowerArray.map(
+      (x) => +x * 1000
+    );
 
     this.filterDto = {
       sortBy: this.filterForm.get('sortByControl')?.value,
       minPrice: this.filterForm.get('minPriceControl')?.value,
       maxPrice: this.filterForm.get('maxPriceControl')?.value,
       powerArray: formattedPowerArray,
-      filterByBrand: this.filterForm.get('filterByBrandControl')?.value !== undefined ?
-                     this.filterForm.get('filterByBrandControl')?.value : null
+      filterByBrand:
+        this.filterForm.get('filterByBrandControl')?.value !== undefined
+          ? this.filterForm.get('filterByBrandControl')?.value
+          : null,
     };
 
-    console.log(this.filterDto);
-
-    if(this.filterDto.maxPrice - this.filterDto.minPrice > 0) {
-      this.airConditionerService.getAllAirConditionersFiltered(this.filterDto)
-      .subscribe((result) => {
-        this.airConditionersFiltered.emit(result.data.airConditioners);
-        this.size.emit(result.size);
-      }, (error) => {
-        console.log(error);
-      });
+    if (this.filterDto.maxPrice - this.filterDto.minPrice > 0) {
+      this.airConditionerService
+        .getAllAirConditionersFiltered(this.filterDto)
+        .subscribe(
+          (result) => {
+            this.airConditionersFiltered.emit(result.data.airConditioners);
+            this.size.emit(result.size);
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
     } else {
       Swal.fire({
-        titleText: this.translate.instant("APP.SWAL.TITLES.ERROR"),
-        text: this.translate.instant("APP.SWAL.MESSAGES.MAX_MIN_PRICE"),
-        icon: "error",
-        confirmButtonText: this.translate.instant("APP.SWAL.BUTTONS.OK"),
-        confirmButtonColor: "#E91E63"
-      })
+        titleText: this.translate.instant('APP.SWAL.TITLES.ERROR'),
+        text: this.translate.instant('APP.SWAL.MESSAGES.MAX_MIN_PRICE'),
+        icon: 'error',
+        confirmButtonText: this.translate.instant('APP.SWAL.BUTTONS.OK'),
+        confirmButtonColor: '#E91E63',
+      });
     }
-
-    
   }
-
 }
